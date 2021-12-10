3ef6011701b5c7aa667fbba876ad2e1cb87f2f62
vrobins <vrobins77@gmail.com>
2021-12-09 20:40:42 -0700

    commit

--- restapi/tests.py
+++ restapi/tests.py
@@ -3,3 +3,10 @@ from unittest import TestCase
 # from django.test import TestCase
 
 # Create your tests here.
+def two_integer_sum(a, b):
+    return a + b
+
+
+class TestSum(TestCase):
+    def test_sum(self):
+        self.assertEqual(two_integer_sum(1, 2), 3)
